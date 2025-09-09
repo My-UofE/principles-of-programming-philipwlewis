32544a7627d43e711c1a791651a40188fba4c010
philipwlewis <philip.lewis@ucl.ac.uk>
2025-09-09 22:05:35 +0000

    sedf

--- .profile/setup.sh
+++ .profile/setup.sh
@@ -2,19 +2,14 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-for PROFILE in /workspaces/*/.profile; do
-   cd "$PROFILE"
-   echo $PROFILE
-   echo $SCRIPT_DIR
-   if [[ $(hostname -s) = codespaces-* ]]; then
-      echo RUNNING CODESPACES SETUP for $PROFILE
-      if [ -f bashrc ]; then
-         mv ~/.bashrc ~/.bashrc.bk >& /dev/null || echo NO PREVIOUS .bashrc
-         ln -s bashrc ~/.bashrc || echo FAILED LINK .bashrc
-      fi
-      if [ -f bash_history ]; then
-         mv ~/.bash_history ~/.bash_history.bk >& /dev/null || echo NO PREVIOUS .bash_history
-         ln -s bash_history ~/.bash_history || echo FAILED LINK .bash_history
-      fi
+if [[ $(hostname -s) = codespaces-* ]]; then
+   echo RUNNING CODESPACES SETUP for $PROFILE
+   if [ -f bashrc ]; then
+      mv ~/.bashrc ~/.bashrc.bk >& /dev/null || echo NO PREVIOUS .bashrc
+      ln -s ~/.profile/bashrc ~/.bashrc || echo FAILED LINK .bashrc
    fi
-done
+   if [ -f bash_history ]; then
+      mv ~/.bash_history ~/.bash_history.bk >& /dev/null || echo NO PREVIOUS .bash_history
+      ln -s ~/.profile/bash_history ~/.bash_history || echo FAILED LINK .bash_history
+   fi
+fi
