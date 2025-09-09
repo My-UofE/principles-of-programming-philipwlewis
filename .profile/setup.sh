412067a1cf6f5d65b135d69892e2a7005f77808f
philipwlewis <philip.lewis@ucl.ac.uk>
2025-09-09 17:26:49 +0000

    another try

--- .profile/setup.sh
+++ .profile/setup.sh
@@ -1,16 +1,18 @@
 #!bash
 
-cd "$1"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd "$SCRIPT_DIR"
 
 if [[ $(hostname -s) = codespaces-* ]]; then
-   echo RUNNING CODESPACES SETUP for $REPO
-   if [ -f ".profile/bashrc" ]; then
+   echo RUNNING CODESPACES SETUP for $SCRIPT_DIR
+   if [ -f bashrc ]; then
       mv ~/.bashrc ~/.bashrc.bk >& /dev/null || echo NO PREVIOUS .bashrc
-      ln -s "$PROFILE/bashrc" ~/.bashrc || echo FAILED LINK .bashrc
+      ln -s bashrc ~/.bashrc || echo FAILED LINK .bashrc
    fi
-   if [ -f ".profile/bash_history" ]; then
+   if [ -f bash_history ]; then
       mv ~/.bash_history ~/.bash_history.bk >& /dev/null || echo NO PREVIOUS .bash_history
-      ln -s "$PROFILE/bash_history" ~/.bash_history || echo FAILED LINK .bash_history
+      ln -s bash_history ~/.bash_history || echo FAILED LINK .bash_history
    fi
 fi
 echo FINSHED CODESPACES SETUP
