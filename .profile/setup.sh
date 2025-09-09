afa434a368f825a499d930fb62355d720a640368
philipwlewis <philip.lewis@ucl.ac.uk>
2025-09-09 15:42:48 +0000

    test again

--- .profile/setup.sh
+++ .profile/setup.sh
@@ -1,17 +1,17 @@
 #!bash
-z
+
 for PROFILE in "/workspaces/*/.profile"; do
 
 if [[ $(hostname -s) = codespaces-* ]]; then
    echo RUNNING CODESPACES SETUP for $REPO
-   if [ -f $PROFILE/bashrc ]; then
+   if [ -f "$PROFILE/bashrc" ]; then
       echo SETTING UP BASHRC
       mv ~/.bashrc ~/.bashrc.bk >& /dev/null || echo NO PREVIOUS .bashrc
       mv ~/.bash_history ~/.bash_history.bk >& /dev/null || echo NO PREVIOUS .bash_history
-      ln -s $PROFILE/bashrc ~/.bashrc || echo FAILED LINK .bashrc
-      ln -s $PROFILE/bash_history ~/.bash_history || echo FAILED LINK .bash_history
+      ln -s "$PROFILE/bashrc" ~/.bashrc || echo FAILED LINK .bashrc
+      ln -s "$PROFILE/bash_history" ~/.bash_history || echo FAILED LINK .bash_history
       echo CODESPACES SETUP fCOMPLETE
    fi
 fi
-echo QUITTING
+echo FINSHED CODESPACES SETUP
 done
